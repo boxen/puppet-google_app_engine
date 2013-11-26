a34a9049d997a3e0edccc6249b0b94bbdb54b988
Quico Moya <me@qmoya.com>
2013-11-26 15:42:56 +0100

    Fix bug

--- manifests/go.pp
+++ manifests/go.pp
@@ -1,4 +1,4 @@
-class google_app_engine::python {
+class google_app_engine::go {
   package { 'GoogleAppEngineGo':
     provider => 'compressed_app',
     source   => 'http://googleappengine.googlecode.com/files/go_appengine_sdk_darwin_amd64-1.8.8.zip',
