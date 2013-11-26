97d122e9c1195b577abfbc729f6f54e05b9df10b
Quico Moya <me@qmoya.com>
2013-11-26 16:19:32 +0100

    Use homebrew for installing the Go App Engine SDK

--- manifests/go.pp
+++ manifests/go.pp
@@ -1,6 +1,5 @@
 class google_app_engine::go {
-  package { 'GoogleAppEngineGo':
-    provider => 'compressed_app',
-    source   => 'http://googleappengine.googlecode.com/files/go_appengine_sdk_darwin_amd64-1.8.8.zip',
+  package { 'go-app-engine-64':
+    ensure => installed
   }
 }
